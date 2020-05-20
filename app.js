04f707d5d95ef6feb663e5d42f352f3a185e6404
Maksim Slyunkov <slyunkovmax@gmail.com>
2020-05-21 01:08:00 +0300

    Выполненный проект

--- app.js
+++ app.js
@@ -1,20 +1,36 @@
 const express = require('express');
 const path = require('path');
-const cards = require('./routes/cards');
-const users = require('./routes/users');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const routerCards = require('./routes/cards');
+const routerUsers = require('./routes/users');
 
 const { PORT = 3000 } = process.env;
-
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5ec59eace4f99f06ace40310',
+  };
+
+  next();
+});
+
+app.use(bodyParser.json());
+app.use('/', routerCards);
+app.use('/', routerUsers);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', cards);
-app.use('/', users);
 
 app.get('*', (req, res) => {
-  res.status(404).send({
-    message: 'Запрашиваемый ресурс не найден',
-  });
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
 
 app.listen(PORT);
--- routes/cards.js
+++ routes/cards.js
@@ -1,18 +1,11 @@
-const cardsRouter = require('express').Router();
-const fsPromises = require('fs').promises;
-const path = require('path');
+const cardRouter = require('express').Router();
 
-const cardsPath = path.join('data', 'cards.json');
+const { createCard, getCards, deleteCard } = require('../controllers/cards');
 
-cardsRouter.get('/cards', (req, res) => {
-  fsPromises.readFile(cardsPath, { encoding: 'utf8' })
-    .then((data) => {
-      res.send(JSON.parse(data));
-    })
-    // eslint-disable-next-line no-unused-vars
-    .catch((err) => {
-      res.status(500).send({ message: 'Запрашиваемый файл не найден' });
-    });
-});
+cardRouter.get('/cards', getCards);
 
-module.exports = cardsRouter;
+cardRouter.post('/cards', createCard);
+
+cardRouter.delete('/cards/:cardId', deleteCard);
+
+module.exports = cardRouter;
